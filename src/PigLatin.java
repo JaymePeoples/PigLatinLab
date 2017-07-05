a96869053e35b7e825098d90da05a9946007263d
JaymePeoples <jaytrax@hotmail.com>
2017-07-05 12:22:02 -0400

    Second commit

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -7,57 +7,67 @@ import java.lang.StringBuffer;
 public class PigLatin {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the Pig Latin Translator!");//1. Display welcome message
-        System.out.println("Please enter a line to be translated: ");//2. Prompt for user word
-        String userInput = scan.nextLine();//3. get user input
-
+        String playAgain;
         //4. translate user input
         //4.1 Convert input to lowercase
-        userInput = userInput.toLowerCase();
-        StringBuffer result = new StringBuffer(userInput);
-        if (isVowel(userInput)) {//vowels
-            result.append("way");
-            //5. display result
-            //6. prompt user to play again
-        } else {//consanent
-            result = piggifiedConsanent(userInput);
-        }
-        System.out.println(result);
-    }
 
-    private static StringBuffer piggifiedConsanent(String userInput) {
-        StringBuffer sBuffer = new StringBuffer(userInput);
+        do {
+            System.out.println("Welcome to the Pig Latin Translator!");//1. Display welcome message
+            System.out.println("Please enter a word to be translated: ");//2. Prompt for user word
 
-        for (int i = 0; i < userInput.length(); i++) {
+            String userInput = scan.nextLine();//3. get user input
+            userInput = userInput.toLowerCase();
+            StringBuffer result = new StringBuffer(userInput);
 
-            if (userInput.charAt(i) == 'a' || userInput.charAt(i) == 'e' || userInput.charAt(i) == 'i' ||
-                    userInput.charAt(i) == 'o' || userInput.charAt(i) == 'u') {
 
-                sBuffer = sBuffer.append(sBuffer.subSequence(0, i) + ("ay")); //add "ay" to end of word
-                sBuffer.delete(0, i);//if word starts with a consanent move all consanents before vowels to end of word
-                return sBuffer;
+            if (isVowel(userInput)) {//vowels
+                result.append("way");
+                //5. display result
+                //6. prompt user to play again
+            } else {//consanent
+                result = piggifiedConsanent(userInput);
             }
-
+            System.out.println(result);
+            System.out.println("Would you like to translate another word? y/n");
+            playAgain = scan.nextLine();
         }
-        return sBuffer;
+        while (playAgain.equalsIgnoreCase("y")) ;
+        System.out.println("Thanks for playing.");
     }
 
 
-    //Translates a word using vowel pig Latin logic
+        private static StringBuffer piggifiedConsanent (String userInput){
+            StringBuffer sBuffer = new StringBuffer(userInput);
+
+            for (int i = 0; i < userInput.length(); i++) {
+
+                if (userInput.charAt(i) == 'a' || userInput.charAt(i) == 'e' || userInput.charAt(i) == 'i' ||
+                        userInput.charAt(i) == 'o' || userInput.charAt(i) == 'u') {
+
+                    sBuffer = sBuffer.append(sBuffer.subSequence(0, i) + ("ay")); //add "ay" to end of word
+                    sBuffer.delete(0, i);//if word starts with a consanent move all consanents before vowels to end of word
+                    return sBuffer;
+                }
+
+            }
+            return sBuffer;
+        }
+
+
+        //Translates a word using vowel pig Latin logic
 //    private static StringBuffer piggifiedVowels(String userInput) {
 //        StringBuffer result;
 //        return re;
 //    }
 
-    //Determines if a word starts with a vowel or consanent
-    private static boolean isVowel(String userInput) {
-        if (userInput.charAt(0) == 'a' || userInput.charAt(0) == 'e' || userInput.charAt(0) == 'i' ||
-                userInput.charAt(0) == 'o' || userInput.charAt(0) == 'u') {
+        //Determines if a word starts with a vowel or consanent
+        private static boolean isVowel (String userInput){
+            if (userInput.charAt(0) == 'a' || userInput.charAt(0) == 'e' || userInput.charAt(0) == 'i' ||
+                    userInput.charAt(0) == 'o' || userInput.charAt(0) == 'u') {
 
-            return true;
-        } else {
-            return false;
+                return true;
+            } else {
+                return false;
+            }
         }
-    }
 }
-
